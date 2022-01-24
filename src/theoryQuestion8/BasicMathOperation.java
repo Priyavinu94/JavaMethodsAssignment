09e52390184aa60664e1c30b1cacc74e62020d7a
Priya <sreepdsy1994@gmail.com>
2022-01-24 14:28:14 -0500

    mistake correction

--- src/theoryQuestion8/BasicMathOperation.java
+++ src/theoryQuestion8/BasicMathOperation.java
@@ -2,8 +2,8 @@ package theoryQuestion8;
 
 public class BasicMathOperation {
 
-	double num1 = 89.62;
-	double num2 = 67.73;
+	double num1 = 0;
+	double num2 = 0;
 
 	double addNum(double num1, double num2) {
 		return num1 + num2;
