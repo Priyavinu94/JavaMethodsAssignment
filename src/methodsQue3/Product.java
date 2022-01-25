bda7cf5c4ef95f730306d782c75225aaf2deb53f
Priya <sreepdsy1994@gmail.com>
2022-01-25 17:37:53 -0500

    Create new package for assignment 2, Ques#3

---
+++ src/methodsQue3/Product.java
@@ -0,0 +1,37 @@
+/* 	1. Initialize 3 variables : Product name, Product description, Product price
+ * 	2. Write methods which just prints these values and call it from main class. 
+ *     (No sys out commands should be present in main class)
+ */
+package methodsQue3;
+
+public class Product {
+	
+	String prodName = "Crock-Pot Digital Slow Cooker 4-qt";
+	String prodDescription = "Features one touch controls and countdown timer\n"
+			+ "Automatically keeps food warm\n"
+			+ "See through tempered glass lid\n"
+			+ "Easy to clean, dishwasher safe stoneware";
+	double prodPrice = 39.99;
+	
+	void setName(String name) {
+		prodName = name;
+	}
+	
+	void setDescription(String description) {
+		prodDescription = description;
+	}
+	
+	void setPrice(double price) {
+		prodPrice = price;
+	}
+	
+	void printName() {
+		System.out.println("Product details\n\nName :	" + prodName);
+	}
+	void printDescription() {
+		System.out.println("\nDescription :\n" + prodDescription);
+	}
+	void printPrice() {
+		System.out.println("\nPrice :	$" + prodPrice);
+	}
+}
---
+++ src/methodsQue3/ProductMain.java
@@ -0,0 +1,15 @@
+package methodsQue3;
+
+public class ProductMain {
+
+	public static void main(String[] args) {
+
+		Product product = new Product();
+		
+		product.printName();
+		product.printDescription();
+		product.printPrice();
+		
+	}
+
+}
