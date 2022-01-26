1dcdb4bef0536ae979095565adf44d7556b34b8a
Priya <sreepdsy1994@gmail.com>
2022-01-25 20:23:36 -0500

    Create new package for Assignment 2, Ques#1

--- src/methodsQue1/Employee.java
+++ src/methodsQue1/Employee.java
@@ -8,39 +8,38 @@
 package methodsQue1;
 
 public class Employee {
-	
-	String firstName = "";	//variable to store the first name of an employee
-	String lastName = "";	//variable to store the last name of an employee
-	double hourlyPay = 0;	//variable to store the hourly pay rate
-	
+
+	String firstName = ""; // variable to store the first name of an employee
+	String lastName = ""; // variable to store the last name of an employee
+	double hourlyPay = 0; // variable to store the hourly pay rate
+
 	void setFirstName(String fName) {
 		firstName = fName;
 	}
-	
+
 	void setLastName(String lName) {
 		lastName = lName;
 	}
-	
+
 	void setHourlyPay(double hPay) {
 		hourlyPay = hPay;
 	}
-	
+
 	String getFirstName() {
 		return firstName;
 	}
-	
+
 	String getLastName() {
 		return lastName;
 	}
-	
+
 	double getHourlyPay() {
 		return hourlyPay;
 	}
-	
-	//method to calculate the weekly salary 
-	double calcWeekPay(double hPay, int noOfHrs) {		
+
+	// method to calculate the weekly salary
+	void calcWeekPay(double hPay, int noOfHrs) {
 		double weeklyPay = hPay * noOfHrs;
 		System.out.println("Weekly salary : $" + weeklyPay);
-		return weeklyPay;
 	}
 }
